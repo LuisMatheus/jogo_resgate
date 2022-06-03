57355f4f1ae7440eea2b282d65b194d4ed6aa298
Matheus <matheusdaeuble@gmail.com>
2022-06-02 21:04:19 -0300

    Add water manager script

---
+++ Assets/Scripts/PathFinder/WaterManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterManager : MonoBehaviour
+{
+
+    public Text waterTimingDisplay;
+    public float waterTiming = 30;
+
+    void Start()
+    {
+        waterTiming = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // waterTimingDisplay.text = waterTiming.ToString();
+        waterTiming = waterTiming - Time.deltaTime;
+
+        if (waterTiming > 0)
+        {
+            waterTimingDisplay.text = waterTiming.ToString();
+        }
+
+        else
+        {
+            waterTimingDisplay.text = "Fim de jogo!";
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("water") == true)
+        {
+            waterTiming = 30;
+            Destroy(col.gameObject);
+        }
+
+
+    }
+
+}
