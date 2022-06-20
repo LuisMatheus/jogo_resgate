ec8e51d37cead9ce4870a5b236cbe6a4b89aaee2
Claudio Santos <clausmoura@edu.unifor.br>
2022-06-19 23:44:02 -0300

    master

--- Assets/Prefabs/Resgatado/ResgatadoScript.cs
+++ Assets/Prefabs/Resgatado/ResgatadoScript.cs
@@ -7,22 +7,26 @@ using Random = UnityEngine.Random;
 
 public class ResgatadoScript : MonoBehaviour
 {
-
-    public int machado = Random.Range(0, 1);
-    public int erva = Random.Range(0, 1);
-    public int corda = Random.Range(0, 1);
-    public int agua = Random.Range(0, 1);
+    public int machado;
+    public int erva;
+    public int corda;
+    public int agua;
     public float vida = 0;
-    
-    
+
+
     public Color cor;
-    
+
     private void Start()
     {
         cor = new Color(
-            Random.Range(0f, 1f), 
-            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
             Random.Range(0f, 1f));
+
+        machado = Random.Range(0, 1);
+        erva = Random.Range(0, 1);
+        corda = Random.Range(0, 1);
+        agua = Random.Range(0, 1);
 
         ParticleSystem.MainModule psMain = GetComponentInChildren<ParticleSystem>().main;
         psMain.startColor = new ParticleSystem.MinMaxGradient(cor, cor);
@@ -32,13 +36,14 @@ public class ResgatadoScript : MonoBehaviour
     void Update()
     {
         vida -= Time.deltaTime;
-        if(vida < 0)
+        if (vida < 0)
         {
             this.GetComponentInParent<ResgatadoSpawnerScript>().removerResgatado(this.gameObject);
         }
     }
 
-    void Setup(int machado,int erva, int corda, int agua,float vida){
+    void Setup(int machado, int erva, int corda, int agua, float vida)
+    {
         this.machado = machado;
         this.erva = erva;
         this.corda = corda;
